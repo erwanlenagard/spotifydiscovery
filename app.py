b4ee97f27d87cb38bb3b5b2bead5b5bfa3bb6993
erwanlenagard <erwan.lenagard@themetricsfactory.com>
2021-08-11 16:23:35 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import os
 from flask import Flask, session, request, redirect
 from flask_session import Session
+from flask_bootstrap import Bootstrap
+
 import spotipy
 import uuid
+
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 Session(app)
+Bootstrap(app)
 
 caches_folder = './.spotify_caches/'
 if not os.path.exists(caches_folder):
